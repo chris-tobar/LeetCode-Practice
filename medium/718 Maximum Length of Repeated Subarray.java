9db1cad20f9f5294afe4e63b570ff411c0088dac
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-20 14:39:12 -0700

    Implementation of comments for code

--- medium/718 Maximum Length of Repeated Subarray.java
+++ medium/718 Maximum Length of Repeated Subarray.java
@@ -25,14 +25,19 @@ class problem718{
 
     public static int findLength(int[] nums1, int[] nums2) {
 
+        //This will keep track of the max number length of subarray found
         int max = 0;
+
+        //Row will keep track of nums1, while Col will keep track of nums2
         int[][] dp = new int[nums1.length + 1][nums2.length+1];
         
-        
+        //Iterate through both of the arrays
         for(int i=0; i<nums1.length; i++)
         {
             for(int j=0; j<nums2.length; j++)
             {
+                //If the number found in nums1 == nums2, then we increment the total subarray found in dp by 1
+                //Then keep track of the max found
                 if(nums1[i] == nums2[j])
                 {
                     dp[i+1][j+1] = dp[i][j] + 1;
@@ -41,6 +46,7 @@ class problem718{
             }
         }
         
+        //Returns the max subarray size found
         return max;
     }
     
