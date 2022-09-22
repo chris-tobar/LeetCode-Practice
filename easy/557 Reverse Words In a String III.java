2e199458b257e0961ac23963660481996dd43a3f
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 12:49:44 -0700

    Comments explaining code

--- easy/557 Reverse Words In a String III.java
+++ easy/557 Reverse Words In a String III.java
@@ -17,22 +17,33 @@ class problem557{
 
     public static String reverseWords(String s) {
         
+        //Take the string and reverse it
         StringBuilder word = new StringBuilder(s);
         word.reverse();
         
+        //new StringBuilder to keep track of the solution
         StringBuilder sol = new StringBuilder();
         
+        //Iterate through StringBuilder starting from the end
         for(int i=word.length()-1, prev=word.length(); i>=0; i--)
         {
+            //White space is encountered, so we want to append it to our solution
             if( Character.isWhitespace( word.charAt(i)) )
             {
+                //We don't want white space to appended at beginning, so we take one more ahead of it
+                //And then append the white space at the end
                 sol.append( word.substring(i+1,prev) + " ");
+
+                //Make prev keep track of the last seen white space
+                //Since prev is exclusive, it will never be apppended at the end
                 prev = i;
             }
+            //Index is 0, so we append the rest of the string, w/out whitespace at the end
             else if(i == 0)
                 sol.append( word.substring(i,prev) );
         }
         
+        //Return our solution
         return sol.toString();
     }
 }
