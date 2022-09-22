214d1f1cb50ad3a7aa19d9632ae4cb55ec2ae9fc
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 13:55:19 -0700

    Addition of problem description

--- medium/985 Sum of Even Numbers After Queries.java
+++ medium/985 Sum of Even Numbers After Queries.java
@@ -1,5 +1,14 @@
 package medium;
 
+/*
+ * You are given an integer array nums and 
+ * an array queries where queries[i] = [vali, indexi].
+
+    For each query i, first, apply 
+    nums[indexi] = nums[indexi] + vali, then print the sum of the even values of nums.
+
+    Return an integer array answer where answer[i] is the answer to the ith query.
+ */
 class problem985{
     
     public static void main(String[] args) {
@@ -20,6 +29,7 @@ class problem985{
 
     }
 
+    //Function to print out the solutions of the array
     public static void print(int[] solution)
     {
         for(int i: solution)
