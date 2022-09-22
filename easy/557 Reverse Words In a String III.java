421c52455638a064d7b7090dc0f891560dc13022
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 12:56:01 -0700

    Addition of problem description

--- easy/557 Reverse Words In a String III.java
+++ easy/557 Reverse Words In a String III.java
@@ -1,5 +1,10 @@
 package easy;
 
+/*
+ * Given a string s, reverse the order of characters 
+ * in each word within a sentence while still preserving 
+ * whitespace and initial word order.
+ */
 class problem557{
     
     public static void main(String[] args) {
