6ea0957d369e7816fbbc5ff8a017d2ddd32fa84f
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 13:27:52 -0700

    Solution done in 29 minutes

--- medium/985 Sum of Even Numbers After Queries.java
+++ medium/985 Sum of Even Numbers After Queries.java
@@ -8,5 +8,38 @@ class problem985{
 
     public static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         
+        int[] solution = new int[nums.length];
+        int solIndex = 0;
+        int evens = 0;
+        
+        for(int i: nums)
+        {
+            if(i%2==0)
+                evens += i;
+        }
+        
+        for(int row=0; row<queries.length; row++)
+        {
+            int index=queries[row][1];
+            int value=queries[row][0];
+            
+            nums[ index ] += value;
+            
+            if(nums[index]%2==0)
+            {
+                if((nums[index]-value)%2==0 )
+                    evens += value;
+                else
+                    evens += nums[index];
+            }
+            else if((nums[index]-value)%2==0) 
+                evens -= nums[index] - value;
+                
+            solution[ solIndex++] = evens;
+            
+        }
+        
+        
+        return solution;
     }
 }
