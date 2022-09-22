4d9bedb708cce1e29a4a01e81f886833c78f56ee
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 12:55:08 -0700

    Creation of new problem being tackled

---
+++ medium/985 Sum of Even Numbers After Queries.java
@@ -0,0 +1,12 @@
+package medium;
+
+class problem985{
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        
+    }
+}
