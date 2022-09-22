eefdbc50cad91bc9ef5f41ca1490bd3be55f8493
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 12:45:31 -0700

    Examples and code created in 15 minutes

--- easy/557 Reverse Words In a String III.java
+++ easy/557 Reverse Words In a String III.java
@@ -4,9 +4,32 @@ class problem557{
     
     public static void main(String[] args) {
         
+        String example1 = "Let's take LeetCode contest";
+        String example2 = "God Ding";
+
+        System.out.println( reverseWords(example1));
+        System.out.println( reverseWords(example2));
+        
     }
 
     public static String reverseWords(String s) {
         
+        StringBuilder word = new StringBuilder(s);
+        word.reverse();
+        
+        StringBuilder sol = new StringBuilder();
+        
+        for(int i=word.length()-1, prev=word.length(); i>=0; i--)
+        {
+            if( Character.isWhitespace( word.charAt(i)) )
+            {
+                sol.append( word.substring(i+1,prev) + " ");
+                prev = i;
+            }
+            else if(i == 0)
+                sol.append( word.substring(i,prev) );
+        }
+        
+        return word.toString();
     }
 }
