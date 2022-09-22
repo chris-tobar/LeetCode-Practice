4cff486dbb8eea88a55278eac6f9bf2dff103c48
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-22 14:05:22 -0700

    Comments explaining code

--- medium/985 Sum of Even Numbers After Queries.java
+++ medium/985 Sum of Even Numbers After Queries.java
@@ -40,38 +40,51 @@ class problem985{
 
     public static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         
+        //Array to store the solution numbers
         int[] solution = new int[nums.length];
+        //Index to iterate through our solution array
         int solIndex = 0;
+        //Int to keep track of the even numbers found
         int evens = 0;
         
+        //Iterate through the array once to count all the even numbers found in beginning
         for(int i: nums)
         {
             if(i%2==0)
                 evens += i;
         }
         
+        //Iterate through all the queries
         for(int row=0; row<queries.length; row++)
         {
+            //Values to keep track of index to change and by what value
             int index=queries[row][1];
             int value=queries[row][0];
             
+            //Add it to the number
             nums[ index ] += value;
             
+            //If the number is even, then we add it to the even counter, depending if it was
+            // even before or odd
             if(nums[index]%2==0)
             {
+                //Number was even before, so we only add the difference
                 if((nums[index]-value)%2==0 )
                     evens += value;
+                //Number was odd before, so we add the whole number
                 else
                     evens += nums[index];
             }
+            //Number went from even to odd, so we decrement by previous number
             else if((nums[index]-value)%2==0) 
                 evens -= nums[index] - value;
                 
+            //Add even number to solution array
             solution[ solIndex++] = evens;
             
         }
         
-        
+        //return the solution array
         return solution;
     }
 }
