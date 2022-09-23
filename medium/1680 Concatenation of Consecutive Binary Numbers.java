5c84ee584beb260c49f79fa09387dbe8eaa8fa50
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-23 11:27:46 -0700

    Adding examples and their solutions

--- medium/1680 Concatenation of Consecutive Binary Numbers.java
+++ medium/1680 Concatenation of Consecutive Binary Numbers.java
@@ -5,9 +5,22 @@ class problem1680{
     
     public static void main(String[] args) {
         
+        //Solution: 1
+        int example1 = 1;
+
+        //Solution: 27
+        int example2 = 3;
+
+        //Solution: 505379714
+        int example3 = 12;
+
+        System.out.println( concatenatedBinary(example1));
+        System.out.println( concatenatedBinary(example2));
+        System.out.println( concatenatedBinary(example3));
+
     }
 
-    public int concatenatedBinary(int n) {
+    public static int concatenatedBinary(int n) {
         
         StringBuilder binary = new StringBuilder();
         
