07bb11e54a3c0927f164e94b691b7eae701d0018
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-23 11:22:04 -0700

    Solution came up with at the end of 35 minutes

--- medium/1680 Concatenation of Consecutive Binary Numbers.java
+++ medium/1680 Concatenation of Consecutive Binary Numbers.java
@@ -1,8 +1,28 @@
 package medium;
+import java.math.BigInteger;
 
 class problem1680{
     
     public static void main(String[] args) {
+        
+    }
+
+    public int concatenatedBinary(int n) {
+        
+        StringBuilder binary = new StringBuilder();
+        
+        for(int i=1; i<=n; i++)
+        {
+            binary.append( Integer.toBinaryString(i) );
+        }
+        
+        BigInteger temp = new BigInteger(binary.toString(), 2);
+        BigInteger mod = new BigInteger("1000000007");
+        temp = temp.mod( mod );
+        
+        
+        return temp.intValue();
+        
         
     }
 }
