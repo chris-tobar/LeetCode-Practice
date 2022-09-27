7a92c6eecb90f3449dfc0c659d283c2b7db47bc7
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-27 11:54:22 -0700

    Solution achieved in 30 minutes
    This solution can solve the problem, but the issue it runs into is the amount of space it takes up, as well as the amount of time it takes to run

--- medium/838 Push Dominoes.java
+++ medium/838 Push Dominoes.java
@@ -4,7 +4,40 @@ class problem838{
 
     public static void main(String[] args)
     {
+        String example1 = "RR.L";
+        String example2 = ".L.R...LR..L..";
 
+        pushDominoes(example2);
     }
-    
+
+    public static String pushDominoes(String dominoes) {
+
+        StringBuilder waves = new StringBuilder(dominoes);
+        StringBuilder prev = new StringBuilder("");
+        
+        while( !waves.toString().equals( prev.toString() ) )
+        {
+            prev = new StringBuilder(waves);
+            
+            for(int i=0; i<waves.length(); i++)
+            {
+                if( (i!=0 && i!=waves.length()-1) && prev.charAt(i)=='.' && 
+                   prev.charAt(i-1)=='R' && prev.charAt(i+1)=='L')
+                {
+                    waves.setCharAt(i,'.');
+                }
+                else if(i!=0 && prev.charAt(i)=='.' && prev.charAt(i-1) == 'R')
+                {
+                    waves.setCharAt(i,'R');
+                }
+                else if(i!=waves.length()-1 && prev.charAt(i)=='.' && prev.charAt(i+1)=='L')
+                {
+                    waves.setCharAt(i,'L');
+                }
+            }
+
+        }
+        
+        return waves.toString();
+    }
 }
