c2091945ca246bae3e5d5413727bfc9bbd6bf608
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-28 17:02:25 -0700

    Code achieved within 10 minutes
    Run time: O(n)
    n = Size of linked list
    We iterate through n to get the size, then do n-requested number
    Therefore it is n+(n-requested number) which simplifies to O(n)

--- medium/19 Remove Nth Node From End of List.java
+++ medium/19 Remove Nth Node From End of List.java
@@ -4,5 +4,33 @@ class problem19{
     
     public static void main(String[] args) {
         
+        
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        int length = 0;
+        ListNode temp = head;
+        
+        while(temp != null)
+        {
+            temp = temp.next;
+            length++;
+        }
+        
+        temp = head;
+        ListNode prev = temp;
+        
+        if(length-n==0)
+            return temp.next;
+        
+        for(int i=0; i<length-n; i++)
+        {
+            prev = temp;
+            temp = temp.next;
+        }
+        
+        prev.next = temp.next;
+        return head;
     }
 }
