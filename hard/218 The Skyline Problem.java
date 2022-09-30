abdc888b87d38b67145a06d79b6fc56c900dfa10
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-09-30 12:51:10 -0700

    Code achieved at end of 45 minutes
    My idea was that we have a hashmap that contains the points and its heights. If they were duplicated, we would take the higher height. If it didn't exist we would see where within the arraylist we should put it, and we would take the heigher height. For the right point we would check to see if it existed, and the take the smaller height.  If it didn't exist we would have taken the height of the point to the the right of it, and take its height.

--- hard/218 The Skyline Problem.java
+++ hard/218 The Skyline Problem.java
@@ -1,8 +1,53 @@
 package hard;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 class problem218{
 
     public static void main(String[] args) {
         
+    }
+
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        
+        List<List<Integer>> sol = new ArrayList<>();
+        
+        //Keeps track of duplicate points within a building
+        HashMap<Integer,Integer> points = new HashMap<>();
+        
+        
+        //Iterate through the buildings within the list
+        for(int i=0; i<buildings.length; i++)
+        {
+            //Left point does not exist, so we will add it to hashmap
+            //Then do some more logic
+            if(!points.containsKey(buildings[i][0]))
+            {
+                points.put(buildings[i][0],buildings[i][2]);
+                
+            }
+            //Point is duplicated so we will take the bigger height and add it to the hashmap
+            else
+            {
+                int hashHeight = points.get(buildings[i][0]);
+                int max = Math.max(hashHeight, buildings[i][2]);
+
+                points.put(buildings[i][0], max);
+            }
+            
+            //Right point does not exist, so we will add it to the hasmap
+            //Then do some more logic 
+            if(!points.containsKey(buildings[i][1]))
+            {
+                System.out.println("temp");
+            }
+            else
+            {
+                System.out.println("temp");
+            }
+        }
+        
+        return sol;
     }
 }
