51e10e3cbbf7bc5027637046b16fc76e75e43f5f
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-03 09:03:10 -0700

    code created after 35 minutes being reached
    The idea of this was to use a priorityQueue to keep track of costs. If duplicated strings were found next to each other, we would add their values to the priorityQueue, and then loop through popping all their values until 1 was left. Then we would iterate from the end of the duplicated substring to avoid repeating.

--- medium/October/1578 Minimum Time to Make Rope Colorful.java
+++ medium/October/1578 Minimum Time to Make Rope Colorful.java
@@ -1,8 +1,48 @@
 package medium.October;
 
+import java.util.PriorityQueue;
+
 class problem1578{
     
     public static void main(String[] args) {
         
+    }
+
+    public int minCost(String colors, int[] neededTime) {
+        
+        PriorityQueue<Integer> values = new PriorityQueue<>();
+        int cost = 0;
+        int sub = 0;
+        
+        for(int i=0; i<colors.length()-1; i++)
+        {
+            if(Character.compare( colors.charAt(i), colors.charAt(i+1))==0  )
+            {
+                sub = subStringLength(colors,i);
+                
+                for(int j=i; j<sub; j++)
+                    values.add( neededTime[j] );
+                
+                while(values.size() > 2)
+                    cost += values.poll();
+                
+                values.clear();
+                i=sub;
+            }
+            
+            
+        }
+        
+        return cost;
+    }
+    
+    public int subStringLength(String colors,int beginning)
+    {
+        int end = beginning;
+        
+        for(int i=beginning; colors.charAt(i)==colors.charAt(beginning) && i<colors.length()-1; i++)
+            end++;
+        
+        return end;
     }
 }
