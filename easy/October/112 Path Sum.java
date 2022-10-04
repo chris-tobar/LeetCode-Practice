b64689daaa27ae64921c43010b43062cbb20229b
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-04 06:27:02 -0700

    Comments added

--- easy/October/112 Path Sum.java
+++ easy/October/112 Path Sum.java
@@ -6,6 +6,9 @@ class problem112{
     
     public static void main(String[] args) {
         
+        TreeNode example1 = new TreeNode();
+        TreeNode example2 = new TreeNode();
+        TreeNode example3 = new TreeNode();
     }
 
     public static boolean hasPathSum(TreeNode root, int targetSum) {
