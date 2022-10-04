62aa7420ebbeadc45d5a114681af338f6db93fb2
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-04 06:02:49 -0700

    Solution achieved within 10 minutes

--- easy/October/112 Path Sum.java
+++ easy/October/112 Path Sum.java
@@ -1,8 +1,22 @@
 package easy.October;
 
+import easy.TreeNode;
+
 class problem112{
     
     public static void main(String[] args) {
         
+    }
+
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if(root == null)
+            return false;
+        
+        if(root.left == null && root.right == null)
+            return targetSum == root.val;
+        
+        return hasPathSum(root.left, targetSum - root.val) ||
+                hasPathSum(root.right, targetSum - root.val);
     }
 }
