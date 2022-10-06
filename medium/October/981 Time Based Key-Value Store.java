ff92057b214189f739e55a0374084877cd38d33e
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-06 11:40:34 -0700

    Solution achieved within 34 minutes

--- medium/October/981 Time Based Key-Value Store.java
+++ medium/October/981 Time Based Key-Value Store.java
@@ -1,9 +1,53 @@
 package medium.October;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 class problem981{
 
     public static void main(String[] args) {
         
+        TimeMap timeMap = new TimeMap();
     }
-    
+
+    public static class TimeMap {
+
+        HashMap<String, TreeMap<Integer,String> > map;
+        
+        public TimeMap() {
+            map = new HashMap<>();
+        }
+        
+        public void set(String key, String value, int timestamp) {
+            
+            if( !map.containsKey(key) )
+            {
+                TreeMap<Integer,String> temp = new TreeMap<>();
+                temp.put(timestamp, value);
+                
+                map.put(key, temp);
+            }
+            else
+            {
+                TreeMap<Integer,String> temp = map.get(key);
+                temp.put(timestamp,value);
+            }
+        }
+        
+        public String get(String key, int timestamp) {
+            
+            if(!map.containsKey(key))
+                return "";
+            
+            TreeMap<Integer,String> temp = map.get(key);
+            
+            if( temp.floorKey(timestamp) == null)
+                return "";
+            
+            Integer floorKey = temp.floorKey(timestamp);
+            
+            return temp.get(floorKey);
+        }
+    }
+
 }
