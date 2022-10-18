32affb0ffef232fd1a39a9ca7ee707795da999c0
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-18 15:26:51 -0700

    code achieved in 35 minutes
    The problem is straight forward when broken into multiple subproblems. First is to break it up and find the number pairings. Next is to convert to a string, and keep repeating that process until we find n-1. The issue I ran into is that I was stumped on figuring out how to find a datastructure or a clever way of setting up the pairings in a way that number pairings are in order, based on when they were inserted. Once that hurdle is taken care of, then subproblem of converting it into a string is straight forward as well.

--- medium/October/38 Count and Say.java
+++ medium/October/38 Count and Say.java
@@ -1,8 +1,37 @@
 package medium.October;
 
+import java.util.LinkedHashMap;
+
 class problem38{
     
     public static void main(String[] args) {
         
+    }
+
+    public static String countAndSay(int n) {
+        
+        return "";
+    }
+    
+    public static LinkedHashMap<Integer,Integer> mapPairings(String number)
+    {
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        
+        for(int i=0; i<number.length(); i++)
+        {
+            int num = number.charAt(i) - '0';
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+        
+        return map;
+    }
+    
+    public static String pairConversion(LinkedHashMap<Integer,Integer> map)
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        
+        
+        return sb.toString();
     }
 }
