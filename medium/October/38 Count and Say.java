66ab36703539baa6e1c2d8141f4de30579bd86ad
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-18 15:43:22 -0700

    adjustment to spacing of problem description

--- medium/October/38 Count and Say.java
+++ medium/October/38 Count and Say.java
@@ -4,11 +4,18 @@ package medium.October;
  * The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
 
     countAndSay(1) = "1"
-    countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+    countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), 
+    which is then converted into a different digit string.
 
-    To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+    To determine how you "say" a digit string, split it into the minimal number of substrings
+     such that each substring contains exactly one unique digit. Then for each substring, 
+     say the number of digits, then say the digit. Finally, concatenate every said digit.
 
     For example, the saying and conversion for digit string "3322251":
+
+        two 3's, three 2's, one 5, and one 1
+        2 3 +       3 2 +   1 5 +   1 1
+                    "23321511"
 
     Given a positive integer n, return the nth term of the count-and-say sequence.
  */
