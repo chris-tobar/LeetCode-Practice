5ab12ab36006ee6e28ca2af93990cf05fa2a1fe2
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-19 10:59:39 -0700

    Logic created w/in 35 minutes
    I was getting very close to the solution this time around. The issue that I came across was figuring out the logic to sort the hashmaps values. Idea was figure out a data structure like a two parameter prioirty queue, then pop k amount of values until we reach our solution.

--- medium/October/692 Top K Frequent Words.java
+++ medium/October/692 Top K Frequent Words.java
@@ -1,8 +1,33 @@
 package medium.October;
 
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 class problem692{
     
     public static void main(String[] args) {
         
+    }
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        
+        HashMap<String,Integer> freq = new HashMap<>();
+        List<String> sol = new ArrayList<>();
+        
+        for(String word: words)
+            freq.put(word, freq.getOrDefault(word,0) + 1);
+        
+        
+        
+        //For loop to iterate through k strings to get our solution
+        for(int i=0; i<k; i++)
+        {
+            
+        }
+        
+        
+        
+        return sol;
     }
 }
