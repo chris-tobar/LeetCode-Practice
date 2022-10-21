9188b3121b27e783b2c955802bc8b939ebf58c7f
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-21 14:35:46 -0700

    Solution achieved in 12 minutes, 18 seconds
    Space Complexity: O(k) where k is the size of the window that we are searching
    
    Time Complexity: O(n) where n is the size of the array

--- easy/October/219 Contains Duplicate II.java
+++ easy/October/219 Contains Duplicate II.java
@@ -1,8 +1,42 @@
 package easy.October;
 
+import java.util.HashSet;
+
 class problem219{
     
     public static void main(String[] args) {
         
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        if(k == 0)
+            return false;
+        
+        HashSet<Integer> dups = new HashSet<>();
+        int left = 0;
+        int right = 1;
+        
+        dups.add( nums[left]);
+        
+        while(right < nums.length)
+        {
+            
+            if( right-left > k)
+            {
+                dups.remove(nums[left]);
+                left++;
+            }
+            
+            if(dups.contains(nums[right]))
+                return true;
+                
+            
+            dups.add( nums[right] );
+            right++;
+            
+        }
+        
+        return false;
     }
 }
