e6321e7624fce420defd6a63bffa35ab361ff2aa
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-21 14:39:45 -0700

    Comments explaining code

--- easy/October/219 Contains Duplicate II.java
+++ easy/October/219 Contains Duplicate II.java
@@ -10,33 +10,43 @@ class problem219{
 
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         
+        //Minimum size needed to solve this problem is 1, thus it will always be false
         if(k == 0)
             return false;
         
+        //HashSet to keep track of duplicate numbers found
         HashSet<Integer> dups = new HashSet<>();
+
+        //Left pointer that will decrease when it reaches size of window
         int left = 0;
+        //Right pointer that will increase size of the window
         int right = 1;
         
+        //Add the left pointer
         dups.add( nums[left]);
         
+        //Iterate through the array until we reach the end, or we found a solution
         while(right < nums.length)
         {
-            
+            //Window size is too big, so we will decrease it
+            //Also get rid of number that it was pointing to
             if( right-left > k)
             {
                 dups.remove(nums[left]);
                 left++;
             }
             
+            //Window size fits, and a duplicate number has been found
             if(dups.contains(nums[right]))
                 return true;
                 
-            
+            //Add the right pointer to the set, before enlarging size of window
             dups.add( nums[right] );
             right++;
             
         }
         
+        //Duplicate number was never found in the size of window requested
         return false;
     }
 }
