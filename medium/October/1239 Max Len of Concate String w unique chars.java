2e716a4bfd605c79172ff05d68f3f7ec44ffe048
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-24 10:50:00 -0700

    Comments explaining code

--- medium/October/1239 Max Len of Concate String w unique chars.java
+++ medium/October/1239 Max Len of Concate String w unique chars.java
@@ -11,26 +11,39 @@ class problem1239{
 
     public static int maxLength(List<String> arr) {
 
+        //ArrayList that will store the length of the strings iterating over
         List<Integer> dp = new ArrayList<>();
+
+        //First string we start with is "", so size is 0
         dp.add(0);
+
+        //Number that will store the max len of string found
         int res = 0;
         
+        //Iterate through every word in the list
         for (String word: arr) {
             
+
             int a = 0;
+
+            //Keeps track of any duplicate letters found within word
             int dup = 0;
             
+            //Iterate through the characters for the word found
             for (char wordChars : word.toCharArray()) 
             {
                 dup |= a & (1 << (wordChars - 'a'));
                 a |= 1 << (wordChars - 'a');
             }
             
+            //String found has duplicate letters, so we will skip over it
             if (dup > 0) 
                 continue;
             
+            //Passed the check above, iterate through to see if our combo has any duplicate chars found
             for (int i = dp.size() - 1; i >= 0; --i) 
             {
+                //Duplicate chars have been found, skip over it 
                 if ((dp.get(i) & a) > 0) 
                     continue;
                 dp.add(dp.get(i) | a);
@@ -38,6 +51,8 @@ class problem1239{
                 res = Math.max(res, Integer.bitCount(dp.get(i) | a));
             }
         }
+
+        //return the max length
         return res;
     }
 
