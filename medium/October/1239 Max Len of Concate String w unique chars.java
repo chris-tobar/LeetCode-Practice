fea489e87d77c0fe288fbd8d4e22e5a926a48146
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-24 10:51:03 -0700

    Problem description added

--- medium/October/1239 Max Len of Concate String w unique chars.java
+++ medium/October/1239 Max Len of Concate String w unique chars.java
@@ -3,6 +3,16 @@ package medium.October;
 import java.util.List;
 import java.util.ArrayList;
 
+/*
+ * You are given an array of strings arr.
+ *  A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+
+    Return the maximum possible length of s.
+
+    A subsequence is an array that can be derived from 
+    another array by deleting some or no elements without changing
+    the order of the remaining elements.
+ */
 class problem1239{
     
     public static void main(String[] args) {
