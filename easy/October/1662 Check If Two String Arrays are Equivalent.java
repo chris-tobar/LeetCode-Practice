464103b4c656b95bb071dee135360bb99b046a7d
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-25 11:44:59 -0700

    Comments explaining code

--- easy/October/1662 Check If Two String Arrays are Equivalent.java
+++ easy/October/1662 Check If Two String Arrays are Equivalent.java
@@ -8,15 +8,22 @@ class problem1662{
 
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
+        //StringBuilder that will store the strings in word1
         StringBuilder wordOne = new StringBuilder();
+
+        //StringBuilder that will store the strings in word2
         StringBuilder wordTwo = new StringBuilder();
         
+        //Iterate through the array and append to first string builder
         for(String temp: word1)
             wordOne.append(temp);
         
+        //Iterate through second array and append to second string builder
         for(String temp: word2)
             wordTwo.append(temp);
         
+        //If the two strings are the same, it will return 0, else it will return -1 or 1
+        //Meaning they differ
         if(wordOne.compareTo(wordTwo) == 0)
             return true;
         else 
