55aca3b72c0f8c5f875276a719e360fefd2246d8
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-25 11:50:44 -0700

    Problem description added

--- easy/October/1662 Check If Two String Arrays are Equivalent.java
+++ easy/October/1662 Check If Two String Arrays are Equivalent.java
@@ -1,5 +1,12 @@
 package easy.October;
 
+/*
+ * Given two string arrays word1 and word2, return true if the two arrays 
+ * represent the same string, and false otherwise.
+
+    A string is represented by an array if the array 
+    elements concatenated in order forms the string.
+ */
 class problem1662{
     
     public static void main(String[] args) {
