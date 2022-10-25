90fd3e25b2afa211be3e97ac735ee164460da8f6
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-25 11:50:10 -0700

    Examples and their solutions

--- easy/October/1662 Check If Two String Arrays are Equivalent.java
+++ easy/October/1662 Check If Two String Arrays are Equivalent.java
@@ -4,6 +4,23 @@ class problem1662{
     
     public static void main(String[] args) {
         
+        String[] example1A = {"ab","c"};
+        String[] example1B = {"a","bc"};
+
+        String[] example2A = {"a","cb"};
+        String[] example2B = {"ab","c"};
+
+        String[] example3A = {"abc","d","defg"};
+        String[] example3B = {"abcddefg"};
+
+
+        System.out.printf( "\nExample1: %b\n", arrayStringsAreEqual(example1A, example1B));
+        System.out.println("Solution: true \n");
+        System.out.printf( "Example2: %b\n",arrayStringsAreEqual(example2A, example2B));
+        System.out.println("Solution: false \n");
+        System.out.printf( "Example3: %b\n", arrayStringsAreEqual(example3A, example3B));
+        System.out.println("Solution: true \n");
+
     }
 
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
