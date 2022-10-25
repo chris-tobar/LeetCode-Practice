00975ac2eba451c1dc12854c4a60c7c0dbd3a43b
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-25 11:43:41 -0700

    Solution achieved in 3 minutes, 52 seconds
    Time Complexity: O(n+m)  n=size of word1, m=size of word2
    
    Space Complexity: O(n+m) n=size of word1, m=size of word2

--- easy/October/1662 Check If Two String Arrays are Equivalent.java
+++ easy/October/1662 Check If Two String Arrays are Equivalent.java
@@ -5,4 +5,21 @@ class problem1662{
     public static void main(String[] args) {
         
     }
+
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+        StringBuilder wordOne = new StringBuilder();
+        StringBuilder wordTwo = new StringBuilder();
+        
+        for(String temp: word1)
+            wordOne.append(temp);
+        
+        for(String temp: word2)
+            wordTwo.append(temp);
+        
+        if(wordOne.compareTo(wordTwo) == 0)
+            return true;
+        else 
+            return false;
+    }
 }
