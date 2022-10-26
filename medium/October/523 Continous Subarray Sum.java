f7b038e346a518ba882aba55af18bbe64ee265d2
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-26 11:37:57 -0700

    Brute force solution
    This can solve the problem, but it can time out for when the problem is a HUGE array.
    
    Time complexity: O(n^2) where n is the size of the array
    
    Space complexity: O(1) since we only used an int

--- medium/October/523 Continous Subarray Sum.java
+++ medium/October/523 Continous Subarray Sum.java
@@ -5,4 +5,22 @@ class problem523{
     public static void main(String[] args) {
         
     }
+
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        
+        for(int i=0; i<nums.length - 1; i++)
+        {
+            int sum = nums[i];
+            
+            for(int j=i+1; j<nums.length; j++)
+            {
+                sum += nums[j];
+                
+                if(sum%k == 0)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
 }
