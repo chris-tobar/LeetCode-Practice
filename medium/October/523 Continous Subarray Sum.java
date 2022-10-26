82113504ff58a6f5cc875eeda07fc09c7cff9ae4
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-26 12:50:31 -0700

    Comments explaining new code

--- medium/October/523 Continous Subarray Sum.java
+++ medium/October/523 Continous Subarray Sum.java
@@ -30,29 +30,40 @@ class problem523{
 
     public static boolean checkSubarraySum(int[] nums, int k) {
         
+        //Hashmap that will keep track of the running remainders
         HashMap<Integer,Integer> map = new HashMap<>();
+
+        //Initalize condition in case the starting value is 0
         map.put(0,-1);
         
         int runningSum = 0;
         
+        //Iterate through the array
         for(int i=0; i<nums.length; i++)
         {
+            //Add number to the running sum
             runningSum += nums[i];
             
+            //Find the remainder IF AND ONLY IF the number we are using is not 0
             if(k!=0)
                 runningSum %= k;
             
+            //See if there exists a number within the map that has the same remainder as our current running sum
             Integer prev = map.get(runningSum);
             
+            //There exists a number within our map that has that remainder
             if(prev != null)
             {
+                //There are two locations that have the same remainder, meaning we found a multiple of our number k
                 if(i - prev > 1)
                     return true;
             }
             else
+                //Remainder doesn't exist yet, OR the size of our window doesn't contain AT LEAST 2 numbers
                 map.put(runningSum, i);
         }
         
+        //No numbers have been found
         return false;
     }
 }
