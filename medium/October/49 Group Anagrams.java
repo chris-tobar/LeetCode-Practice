64ac77e6a9b47594bca3885752ef3a9b215ea957
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-27 18:35:47 -0700

    Problem description added

--- medium/October/49 Group Anagrams.java
+++ medium/October/49 Group Anagrams.java
@@ -5,6 +5,14 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+/*
+ * Given an array of strings strs, group the anagrams together. 
+ * You can return the answer in any order.
+
+    An Anagram is a word or phrase formed by rearranging 
+    the letters of a different word or phrase, typically
+    using all the original letters exactly once.
+ */
 class problem49{
     
     public static void main(String[] args) {
