b6caa6801b3a9a829fa19189bd4ca996c84064c4
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-27 18:24:15 -0700

    Solution achieved in 10 minutes

--- medium/October/49 Group Anagrams.java
+++ medium/October/49 Group Anagrams.java
@@ -1,8 +1,32 @@
 package medium.October;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 class problem49{
     
     public static void main(String[] args) {
         
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+  
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for(String word: strs)
+        {
+            char[] ca = word.toCharArray();
+            Arrays.sort(ca);
+            String keyString = String.valueOf(ca);
+            
+            if(!map.containsKey(keyString))
+                map.put(keyString, new ArrayList<>());
+            
+            map.get(keyString).add(word);
+        }
+        
+        return new ArrayList<>( map.values() );
     }
 }
