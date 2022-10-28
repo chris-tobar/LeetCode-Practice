96ed63e9bd714deaf0002420fcc61b4c831de8fd
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-28 09:01:00 -0700

    Solution for problem 839
    Had some issues starting up the problem, as I didn't know how to begin. After watching a youtube video, approach is similar to a BS in terms of the helper function, and we are testing all combinations of both matrixes to find our max.
    
    Time complexity: O(n^2)
    Where n is the size of the array. Time complexity would take longer since we are actually doing 2n^2n, but we can drop the constants.
    
    Space complexity: O(1)
    No space was used

--- medium/October/835 Image Overlap.java
+++ medium/October/835 Image Overlap.java
@@ -5,4 +5,39 @@ class problem835{
     public static void main(String[] args) {
         
     }
+
+    public static int largestOverlap(int[][] img1, int[][] img2) {
+        
+        int overlap = 0;
+        
+        for(int i= -img1.length + 1; i<img1.length; i++)
+        {
+            for(int j= -img1[0].length+1; j<img1[0].length; j++)
+            {
+                overlap = Math.max(overlap, countOverlap(img1,img2,i,j));
+            }
+        }
+        
+        return overlap;
+        
+    }
+    
+    private static int countOverlap(int[][] img1, int[][] img2, int rowOffset, int colOffset)
+    {
+        int count = 0;
+        
+        for(int row = 0; row<img1.length; row++)
+        {
+            for(int col=0; col< img1[0].length; col++)
+            {
+                if(row+rowOffset<0 || row+rowOffset >= img1.length || col+colOffset<0 || col+colOffset >= img1[0].length)
+                    continue;
+                
+                if(img1[row][col] + img2[row+rowOffset][col+colOffset] == 2)
+                    count++;
+            }
+        }
+        
+        return count;
+    }
 }
