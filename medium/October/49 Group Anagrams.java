8006fb85e5a2814874dbe3455e12bcefd4c3bd82
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-27 18:28:02 -0700

    Comments explaining solution
    Space complexity: O(n) where n is the array size
    
    Time complexity: O(n log n)

--- medium/October/49 Group Anagrams.java
+++ medium/October/49 Group Anagrams.java
@@ -13,20 +13,28 @@ class problem49{
 
     public static List<List<String>> groupAnagrams(String[] strs) {
   
+        //Have a hashmap store the buckets, and a way to identify them
         HashMap<String, List<String>> map = new HashMap<>();
         
+        //Iterate through the array
         for(String word: strs)
         {
+            //Convert the string into a character array
             char[] ca = word.toCharArray();
+            //Sort the array
             Arrays.sort(ca);
+            //Get the String value of that character array
             String keyString = String.valueOf(ca);
             
+            //Value doesn't exist, so we create a new list for that value
             if(!map.containsKey(keyString))
                 map.put(keyString, new ArrayList<>());
             
+            //Get the list, and add the string to it
             map.get(keyString).add(word);
         }
         
+        //Convert all the values in the map, into an array list
         return new ArrayList<>( map.values() );
     }
 }
