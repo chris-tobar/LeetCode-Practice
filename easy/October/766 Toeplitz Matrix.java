f01146015a876c23417e6eee928109a3f07d05e7
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-31 11:20:50 -0700

    Comment explaining code

--- easy/October/766 Toeplitz Matrix.java
+++ easy/October/766 Toeplitz Matrix.java
@@ -9,15 +9,19 @@ class problem766{
 
     public static boolean isToeplitzMatrix(int[][] matrix) {
         
+        //Iterate through the array len -1 so we don't check corners and go out of bounds
         for (int i = 0; i < matrix.length - 1; i++) 
         {
+            //Same logic as above, len - 1 so we don't check corners and go out of bounds
             for (int j = 0; j < matrix[i].length - 1; j++) 
             {
+                //Number in the diagnals don't match up, return false
                 if (matrix[i][j] != matrix[i + 1][j + 1]) 
                     return false;
             }
         }
         
+        //All diagnals have the same number, return true
         return true;
     }
 }
