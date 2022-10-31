a6042b047a5cc3329da69faee3c0ff3bd91bea6e
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-31 11:19:41 -0700

    More elegant and simplified solution
    Problem is able to be solved with just 2 for loops. All it doesn't check the corners since they're always going to be true for them, hence the len-1 checks in the for loops.
    
    Space complexity: O(1) no space was used
    
    Time complexity: O(M x N) where m is the number of rows, and N is the number of columns. For every row we iterate through all the columns to check.

--- easy/October/766 Toeplitz Matrix.java
+++ easy/October/766 Toeplitz Matrix.java
@@ -9,49 +9,15 @@ class problem766{
 
     public static boolean isToeplitzMatrix(int[][] matrix) {
         
-        int rowLength = matrix.length;
-        int colLength = matrix[0].length;
-        
-        //Below mid line
-        for(int row=1; row< rowLength; row++)
+        for (int i = 0; i < matrix.length - 1; i++) 
         {
-            int curr = matrix[row][0];
-            
-            for(int col=0; col< colLength; col++)
+            for (int j = 0; j < matrix[i].length - 1; j++) 
             {
-                if(row+col >= rowLength)
-                    break;
-                
-                if(matrix[row+col][col] != curr)
+                if (matrix[i][j] != matrix[i + 1][j + 1]) 
                     return false;
             }
         }
         
-        //MidLine
-        for(int row=0; row< rowLength; row++)
-        {
-            if(matrix[row][row] != matrix[0][0])
-                return false;
-        }
-        
-        //upper mid Line
-        for(int row=0; row < rowLength-1; row++)
-        {
-            //TODO FIX BUG
-            int curr = matrix[0][row+1];
-            
-            for(int col=1; col < colLength; col++)
-            {
-                if(row+col-1 >= rowLength)
-                    break;
-                
-                if(matrix[col-1][col] != curr)
-                    return false;
-            }
-        }
-        
-        
-    
         return true;
     }
 }
