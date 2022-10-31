f6a8ef187509a09a60d580afdc26e81499903bc0
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-31 11:09:49 -0700

    solution written at end of 25 minutes
    Code is close to completion. Logic is missing from the upper mid loop, and once that is figured out, problem can be solved.

--- easy/October/766 Toeplitz Matrix.java
+++ easy/October/766 Toeplitz Matrix.java
@@ -6,4 +6,52 @@ class problem766{
         
         System.out.println("Hello world!");
     }
+
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+        
+        //Below mid line
+        for(int row=1; row< rowLength; row++)
+        {
+            int curr = matrix[row][0];
+            
+            for(int col=0; col< colLength; col++)
+            {
+                if(row+col >= rowLength)
+                    break;
+                
+                if(matrix[row+col][col] != curr)
+                    return false;
+            }
+        }
+        
+        //MidLine
+        for(int row=0; row< rowLength; row++)
+        {
+            if(matrix[row][row] != matrix[0][0])
+                return false;
+        }
+        
+        //upper mid Line
+        for(int row=0; row < rowLength-1; row++)
+        {
+            //TODO FIX BUG
+            int curr = matrix[0][row+1];
+            
+            for(int col=1; col < colLength; col++)
+            {
+                if(row+col-1 >= rowLength)
+                    break;
+                
+                if(matrix[col-1][col] != curr)
+                    return false;
+            }
+        }
+        
+        
+    
+        return true;
+    }
 }
