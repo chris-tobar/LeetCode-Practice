796b3678c184a0fe2eaa71919941b1e1c5860903
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-01 14:52:39 -0700

    Solution at end of 35 minutes
    Error in logic in one of the if statements, because it goes out of bounds. I tried to check for end of boundaries issues, as well as if there was a v and the ball could no longer drop.
    
    If done correctly:
    Time complexity: O(M x N) where M is number of rows, and N is number of columns
    
    Space complexity: O(1) no extra space was used

--- medium/November/1706 Where will the Ball Fall.java
+++ medium/November/1706 Where will the Ball Fall.java
@@ -5,4 +5,44 @@ class problem1706{
     public static void main(String[] args) {
         
     }
+
+    public static int[] findBall(int[][] grid) {
+        
+        int[] sol = new int[grid[0].length];
+        
+        int index = 0;
+        int row = 0;
+        int col = 0;
+        
+        while(index < grid[0].length)
+        {
+            
+            if(col < 0 || col >= grid[0].length)
+            {
+                sol[index++] = -1;
+                row = 0;
+                col = index;
+            }
+            else if(col>=grid[0].length-2 && 
+                    (grid[row][col]==1 && grid[row][col+1] ==-1) )
+            {
+                sol[index++] = -1;
+                row = 0;
+                col = index;
+            }
+            else if(col >= grid.length)
+            {
+                sol[index++] = col;
+                row = 0;
+                col = index;
+            }
+            else
+            {
+                col += grid[row][col];
+                row++;
+            }
+        }
+        
+        return sol;
+    }
 }
