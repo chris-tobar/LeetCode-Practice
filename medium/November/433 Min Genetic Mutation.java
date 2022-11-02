bd83bc42b890999b9ec8db828e1e649f67c2e424
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-02 12:37:22 -0700

    Code achieved at end of 35 minutes
    All the checks are in place to see if its valid to check through the entire bank. Issue arises w/ checking if there is a valid mutation w/in the bank.
    
    There is an error in logic within converting the start to end.
    
    If done correctly:
    
    Space complexity: O(n) where n is the size of the bank
    
    Time complexity: O(n) where n is the size of the bank

--- medium/November/433 Min Genetic Mutation.java
+++ medium/November/433 Min Genetic Mutation.java
@@ -1,8 +1,69 @@
 package medium.November;
 
+import java.util.HashSet;
 class problem433{
     
     public static void main(String[] args) {
         
+    }
+
+    public static int minMutation(String start, String end, String[] bank) {
+        
+        int charCount = 0;
+        boolean endInBank = false;
+        HashSet<Integer> indexToSkip = new HashSet<>();
+        
+        if(bank.length == 0)
+            return start.equals(end) ? 0 : -1;
+        else if(start.equals(end))
+            return 0;
+        
+        charCount = charsToChange(start,end);
+
+        for(String gene: bank)
+        {
+            if(gene.equals(end))
+                endInBank = true;
+        }
+        
+        if(charCount > bank.length || !endInBank)
+            return - 1;
+        
+        //CHECKS BANK NOW TO SEE IF MODIFICATION IS AVAILABLE
+        for(int i=0; i< charCount; i++)
+        {
+            for(int j=0; j<bank.length; j++)
+            {
+                if(start.equals(end))
+                    break;
+                
+                //SKip over index since we mutated through it
+                if(indexToSkip.contains(j))
+                    continue;
+                
+                if(charsToChange(start, bank[j]) == 1)
+                {
+                    start = bank[j];
+                    indexToSkip.add(j);
+                }
+                
+            }
+        }
+    
+        return start.equals(end) ? charCount : -1;
+    }
+    
+    public static int charsToChange(String start, String end)
+    {
+        
+        int count = 0;
+        
+        for(int i=0; i<8; i++)
+        {
+            if(start.charAt(i) != end.charAt(i))
+                count++;
+        }
+        
+        return count;
     }
 }
