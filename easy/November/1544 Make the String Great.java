5a9c1f4553c9968272ab57908be31dfded5ef26c
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-08 11:20:11 -0800

    Code achieved at end of 25 minutes
    This can only solve a select few cases. Since solution was done iteratively, we never fully achieve solution for some cases since we don't check the string again to see if its proper. Can be solved properly if done with a while loop to check if there are repeats.
    
    Time complexity: O(n) where n is the length of the string
    
    Space complexity: O(1) we only use basic variables, so no space is being used

--- easy/November/1544 Make the String Great.java
+++ easy/November/1544 Make the String Great.java
@@ -4,6 +4,38 @@ class problem1544{
 
     public static void main(String[] args)
     {
+
+    }
+
+    public static String makeGood(String s) {
         
+        if(s.length() == 1)
+            return s;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<s.length()-1; i++)
+        {
+            Character letter = Character.toLowerCase( s.charAt(i) ) ;
+            Character letterAhead = Character.toLowerCase( s.charAt(i+1) );
+            
+            if( letter.equals(letterAhead) )
+            {
+                Character temp = s.charAt(i);
+                Character temp2 = s.charAt(i+1);
+                
+                if( !temp.equals(temp2) )
+                    i++;
+                else
+                    sb.append( s.charAt(i));
+            }
+            else
+                sb.append( s.charAt(i));
+            
+        }
+        
+        sb.append(s.charAt( s.length() -1 ) );
+        
+        return sb.toString();
     }
 }
