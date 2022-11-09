2f7c44952a7045cec4c96cf665b428dd845fe584
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-09 13:01:50 -0800

    Push of code achieved at end of 35 minutes
    This solution can solve the problem, but it can run into an issue when the problem size gets too big, because we are constantly, copying over a stack. Because of this, it only passes 88/96 cases.
    
    Space complexity: O(n) where n is the size of the stack
    
    Time complexity: O(n^2) where n is the size of the stack

--- medium/November/901 Online Stock Span.java
+++ medium/November/901 Online Stock Span.java
@@ -1,8 +1,56 @@
 package medium.November;
 
+import java.util.Stack;
+
 class problem901{
     
     public static void main(String[] args) {
         
+        StockSpanner example1 = new StockSpanner();
+    }
+
+    public static class StockSpanner {
+
+        Stack<Integer> stack;
+        int prevMax;
+        int prevNumber;
+        
+        public StockSpanner() {
+            this.stack = new Stack<>();
+            this.prevMax = 1;
+            this.prevNumber = 1;
+        }
+        
+        public int next(int price) {
+            
+            if(stack.isEmpty())
+            {
+                stack.push(price);
+                return 1;
+            }
+            else if( price < stack.peek() )
+            {
+                stack.push(price);
+                this.prevMax = 1;
+                this.prevNumber = price;
+                return 1;
+            }
+            else
+                stack.push(price);
+            
+            Stack<Integer> temp = (Stack<Integer>)stack.clone();
+            int max = 0;
+            
+            while(!temp.isEmpty())
+            {
+                if( temp.pop() <= price)
+                    max++;
+                else
+                    return max;
+            }
+            
+            
+            return max;
+        }
     }
 }
