69b900312424788f9521d9c2e13b71026e2d623d
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-09 15:11:31 -0800

    Comments explaining code

--- medium/November/901 Online Stock Span.java
+++ medium/November/901 Online Stock Span.java
@@ -11,20 +11,29 @@ class problem901{
 
     static class StockSpanner {
 
+        //Stack that will hold pair values of PRICE and COUNT
         Stack<int[]> stack;
         
+        //Initalize the stack
         public StockSpanner() {
             this.stack = new Stack<>();
         }
         
         public int next(int price) {
             
+            //Since the starting price is always counted, we start at 1
             int count = 1;
             
+            //While there are still items in the stack AND top of stack is <= to current price
+            // Append that prices count to the current count
             while(!stack.isEmpty() && stack.peek()[0] <= price)
                 count += stack.pop()[1];
             
+            //No more items left in stack, or value top of stack is bigger than current price
+            //Push the value pair into the stack
             stack.push( new int[] {price, count} );
+
+            //Return the count found 
             return count;
         }
     }
