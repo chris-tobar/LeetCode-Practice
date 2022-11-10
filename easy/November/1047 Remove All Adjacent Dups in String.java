8d85d4b9185960a53867ec10f039a4f5d9436e2d
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-11-10 11:42:35 -0800

    Solution achieved in 6 minutes, 14 seconds
    Idea is to push and pop characters on the stack. We are at most adding to the stack, or removing from the top.
    
    Space complexity: O(n) where n is size of the string.
    
    Time complexity: O(2n) -> O(n)
    where n is the size of the string

--- easy/November/1047 Remove All Adjacent Dups in String.java
+++ easy/November/1047 Remove All Adjacent Dups in String.java
@@ -1,9 +1,33 @@
 package easy.November;
 
+import java.util.Stack;
+
 class problem1047{
     
     public static void main(String[] args)
     {
+
+    }
+
+    public static String removeDuplicates(String s) {
         
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            if(!stack.isEmpty() && s.charAt(i)==stack.peek())
+                stack.pop();
+            else
+                stack.push( s.charAt(i));
+        }
+        
+        int index = stack.size() - 1;
+        char[] word = new char[stack.size()];
+        
+        while(!stack.isEmpty())
+            word[index--] = stack.pop();
+            
+            
+        return new String(word);
     }
 }
